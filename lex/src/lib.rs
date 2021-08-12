35d920d44ef2338102831ac35cb87496579e0715
Adam <adam.and.math@gmail.com>
2021-08-13 01:44:26 +0200

    Extract state-machine behaviour into state methods

--- lex/src/lib.rs
+++ lex/src/lib.rs
@@ -133,9 +133,11 @@ impl Span {
 }*/
 
 #[derive(Debug)]
-enum State {
+enum State<'a> {
     // The empty state - nothing have been parsed yet
     Empty,
+    // We found a token
+    Sure(Token<'a>),
     // We have encountered whitespace, and are gonna parse until something else is encountered
     WhiteSpace,
     // We have encountered something that may either be an identifier or a keyword
@@ -143,10 +145,9 @@ enum State {
     MinusOrArrow,
     IntOrFloat,
     OptionFloat,
-    StringLiteral,
-    StringLiteralEscaped,
     Float,
-    Int,
+    StringLiteral,
+    CompleteStringLiteral,
 }
 
 impl<'a> Iterator for TokenStream<'a> {
@@ -156,385 +157,137 @@ impl<'a> Iterator for TokenStream<'a> {
         let input = &self.input[self.index..];
         let mut indecies = input.char_indices();
         let mut state = State::Empty;
-        let mut token_str = &input[..0];
         let start = self.pos;
 
         while let Some((p, c)) = indecies.next() {
-            match state {
-                State::Empty => match c {
-                    'a'..='z' | 'A'..='Z' | '_' => state = State::KeyOrIdent,
-                    '0'..='9' => state = State::IntOrFloat,
-                    '(' => {
-                        self.index += p + 1;
-                        self.pos.column += 1;
-                        return Some((
-                            Token::Bracket(Bracket::Start),
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                    ')' => {
-                        self.index += p + 1;
-                        self.pos.column += 1;
-                        return Some((
-                            Token::Bracket(Bracket::End),
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                    '[' => {
-                        self.index += p + 1;
-                        self.pos.column += 1;
-                        return Some((
-                            Token::Square(Bracket::Start),
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                    ']' => {
-                        self.index += p + 1;
-                        self.pos.column += 1;
-                        return Some((
-                            Token::Square(Bracket::End),
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                    '{' => {
-                        self.index += p + 1;
-                        self.pos.column += 1;
-                        return Some((
-                            Token::Curley(Bracket::Start),
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                    '}' => {
-                        self.index += p + 1;
-                        self.pos.column += 1;
-                        return Some((
-                            Token::Curley(Bracket::End),
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                    ':' => {
-                        self.index += p + 1;
-                        self.pos.column += 1;
-                        return Some((
-                            Token::Colon,
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                    ';' => {
-                        self.index += p + 1;
-                        self.pos.column += 1;
-                        return Some((
-                            Token::Semi,
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                    ',' => {
-                        self.index += p + 1;
-                        self.pos.column += 1;
-                        return Some((
-                            Token::Comma,
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                    '=' => {
-                        self.index += p + 1;
-                        self.pos.column += 1;
-                        return Some((
-                            Token::Eq,
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                    '+' => {
-                        self.index += p + 1;
-                        self.pos.column += 1;
-                        return Some((
-                            Token::Plus,
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                    '-' => {
-                        state = State::MinusOrArrow;
-                    }
-                    '.' => {
-                        self.index += p + 1;
-                        self.pos.column += 1;
-                        return Some((
-                            Token::Dot,
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                    '"' => {
-                        state = State::StringLiteral;
-                    }
-                    c if c.is_whitespace() => state = State::WhiteSpace,
-                    c => {
-                        let skip_bytes = c.len_utf8();
-                        self.index += p + skip_bytes;
-                        self.pos.column += 1;
-                        return Some((
-                            Token::Error,
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                },
-                State::WhiteSpace => {
-                    if c.is_whitespace() {
-                    } else {
-                        self.index += p;
-                        return Some((
-                            Token::WhiteSpace,
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                }
-                State::KeyOrIdent => match c {
-                    'a'..='z' | 'A'..='Z' | '_' | '0'..='9' => {}
-                    _ => {
-                        self.index += p;
-                        if let Ok(k) = KeyWord::try_from(token_str) {
-                            return Some((
-                                Token::KeyWord(k),
-                                Span {
-                                    start,
-                                    end: self.pos,
-                                },
-                            ));
-                        } else {
-                            return Some((
-                                Token::Ident(token_str),
-                                Span {
-                                    start,
-                                    end: self.pos,
-                                },
-                            ));
-                        }
-                    }
-                },
-                State::MinusOrArrow => match c {
-                    '>' => {
-                        self.index += p + 1;
-                        self.pos.column += 1;
-                        return Some((
-                            Token::Arrow,
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                    _ => {
-                        self.index += p;
-                        return Some((
-                            Token::Minus,
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                },
-                State::IntOrFloat => match c {
-                    '0'..='9' | '_' => {}
-                    'a'..='z' | 'A'..='Z' => state = State::Int,
-                    '.' => state = State::OptionFloat,
-                    _ => {
-                        self.index += p;
-                        return Some((
-                            Token::Int(token_str),
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                },
-                State::Int => match c {
-                    'a'..='z' | 'A'..='Z' | '0'..='9' | '_' => {}
-                    _ => {
-                        self.index += p;
-                        return Some((
-                            Token::Int(token_str),
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                },
-                State::OptionFloat => match c {
-                    '0'..='9' => state = State::Float,
-                    'a'..='z' | 'A'..='Z' | '_' => {
-                        self.index += p - 1;
-                        self.pos.column -= 1;
-                        return Some((
-                            Token::Int(&token_str[..token_str.len() - 1]),
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ));
-                    }
-                    _ => {
-                        return Some((
-                            Token::Float(token_str),
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ))
-                    }
-                },
-                State::Float => match c {
-                    '0'..='9' | 'a'..='z' | 'A'..='Z' | '_' => {}
-                    _ => {
-                        return Some((
-                            Token::Float(token_str),
-                            Span {
-                                start,
-                                end: self.pos,
-                            },
-                        ))
-                    }
-                },
-                State::StringLiteral => match c {
-                    '"' => {
-                        self.index += p + 1;
-                        self.pos.column += 1;
-                        return match unescape_string(Cow::Borrowed(&token_str[1..])) {
-                            Some(s) => Some((
-                                Token::StringLiteral(s),
-                                Span {
-                                    start,
-                                    end: self.pos,
-                                },
-                            )),
-                            None => Some((
-                                Token::Error,
-                                Span {
-                                    start,
-                                    end: self.pos,
-                                },
-                            )),
-                        };
-                    }
-                    '\\' => state = State::StringLiteralEscaped,
-                    _ => {}
-                },
-                State::StringLiteralEscaped => match c {
-                    _ => state = State::StringLiteral,
-                },
-            }
-
-            token_str = &input[..=p];
+            let p = p + 1;
             if c == '\n' {
-                self.pos.line += 1;
                 self.pos.column = 0;
+                self.pos.line += 1;
             } else {
                 self.pos.column += 1;
             }
+
+            let (s, done) = state.next(c);
+            state = s;
+
+            if let Some(i) = done {
+                self.index += p - i;
+                self.pos = Pos {
+                    line: self.pos.line,
+                    column: self.pos.column - i,
+                };
+                let token = state.token(&input[0..p - i])?;
+                let span = Span {
+                    start,
+                    end: self.pos,
+                };
+                return Some((token, span));
+            }
         }
 
-        // No characters were left in the string
         self.index = self.input.len();
-        match state {
-            State::Empty => None,
-            State::KeyOrIdent => {
+        let token = state.token(input)?;
+        let span = Span {
+            start,
+            end: self.pos,
+        };
+        Some((token, span))
+    }
+}
+
+impl<'a> State<'a> {
+    fn next(self, c: char) -> (Self, Option<usize>) {
+        use State::*;
+        match self {
+            Sure(_) => unreachable!(),
+            Empty => match c {
+                'a'..='z' | 'A'..='Z' | '_' => (KeyOrIdent, None),
+                '0'..='9' => (IntOrFloat, None),
+                '(' => (Sure(Token::Bracket(Bracket::Start)), Some(0)),
+                ')' => (Sure(Token::Bracket(Bracket::End)), Some(0)),
+                '[' => (Sure(Token::Square(Bracket::Start)), Some(0)),
+                ']' => (Sure(Token::Square(Bracket::End)), Some(0)),
+                '{' => (Sure(Token::Curley(Bracket::Start)), Some(0)),
+                '}' => (Sure(Token::Curley(Bracket::End)), Some(0)),
+                ':' => (Sure(Token::Colon), Some(0)),
+                ';' => (Sure(Token::Semi), Some(0)),
+                ',' => (Sure(Token::Comma), Some(0)),
+                '=' => (Sure(Token::Eq), Some(0)),
+                '+' => (Sure(Token::Plus), Some(0)),
+                '-' => (MinusOrArrow, None),
+                '.' => (Sure(Token::Dot), Some(0)),
+                '"' => (StringLiteral, None),
+                c if c.is_whitespace() => (WhiteSpace, None),
+                _ => (Sure(Token::Error), Some(0)),
+            },
+            WhiteSpace => (
+                WhiteSpace,
+                match c {
+                    c if c.is_whitespace() => None,
+                    _ => Some(1),
+                },
+            ),
+            KeyOrIdent => (
+                KeyOrIdent,
+                match c {
+                    'a'..='z' | 'A'..='Z' | '_' | '0'..='9' => None,
+                    _ => Some(1),
+                },
+            ),
+            MinusOrArrow => match c {
+                '>' => (Sure(Token::Arrow), Some(0)),
+                _ => (MinusOrArrow, Some(1)),
+            },
+            IntOrFloat => match c {
+                '0'..='9' | '_' => (IntOrFloat, None),
+                '.' => (OptionFloat, None),
+                _ => (IntOrFloat, Some(1)),
+            },
+            OptionFloat => match c {
+                '0'..='9' => (Float, None),
+                'a'..='z' | 'A'..='Z' | '_' => (IntOrFloat, Some(2)),
+                _ => (Float, Some(1)),
+            },
+            Float => (
+                Float,
+                match c {
+                    '0'..='9' => None,
+                    _ => Some(1),
+                },
+            ),
+            StringLiteral => match c {
+                '"' => (CompleteStringLiteral, Some(0)),
+                _ => (StringLiteral, None),
+            },
+            CompleteStringLiteral => unreachable!(),
+        }
+    }
+
+    fn token(self, token_str: &'a str) -> Option<Token<'a>> {
+        use State::*;
+        Some(match self {
+            Empty => return None,
+            Sure(token) => token,
+            WhiteSpace => Token::WhiteSpace,
+            KeyOrIdent => {
                 if let Ok(k) = KeyWord::try_from(token_str) {
-                    Some((
-                        Token::KeyWord(k),
-                        Span {
-                            start,
-                            end: self.pos,
-                        },
-                    ))
+                    Token::KeyWord(k)
                 } else {
-                    Some((
-                        Token::Ident(token_str),
-                        Span {
-                            start,
-                            end: self.pos,
-                        },
-                    ))
+                    Token::Ident(token_str)
                 }
             }
-            State::WhiteSpace => Some((
-                Token::WhiteSpace,
-                Span {
-                    start,
-                    end: self.pos,
-                },
-            )),
-            State::MinusOrArrow => Some((
-                Token::Minus,
-                Span {
-                    start,
-                    end: self.pos,
-                },
-            )),
-            State::IntOrFloat | State::Int => Some((
-                Token::Int(token_str),
-                Span {
-                    start,
-                    end: self.pos,
-                },
-            )),
-            State::OptionFloat | State::Float => Some((
-                Token::Float(token_str),
-                Span {
-                    start,
-                    end: self.pos,
-                },
-            )),
-            State::StringLiteral | State::StringLiteralEscaped => Some((
-                Token::Error,
-                Span {
-                    start,
-                    end: self.pos,
-                },
-            )),
-        }
+            MinusOrArrow => Token::Minus,
+            IntOrFloat => Token::Int(token_str),
+            OptionFloat => Token::Float(token_str),
+            Float => Token::Float(token_str),
+            StringLiteral => Token::Error,
+            CompleteStringLiteral => {
+                unescape_string(Cow::Borrowed(&token_str[1..token_str.len() - 1]))
+                    .map(Token::StringLiteral)
+                    .unwrap_or(Token::Error)
+            }
+        })
     }
 }
 
